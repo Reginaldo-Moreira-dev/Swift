5e51f2e7445ffe03c649e34f1be75854ee4d85df
Reginaldo Moreira <139932119+Reginaldo-Moreira-dev@users.noreply.github.com>
2024-04-23 21:59:42 -0300

    Initial Commit

---
+++ App01/App01App.swift
@@ -0,0 +1,17 @@
+//
+//  App01App.swift
+//  App01
+//
+//  Created by Dev Regis on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct App01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App01
+//
+//  Created by Dev Regis on 23/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
